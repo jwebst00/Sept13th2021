3ace8d58a77ba4f49b908838d25865daca0cdb81
Jackson Webster <jwebst00@g.uafs.edu>
2021-09-13 15:50:14 +0000

    adjusted values

--- boolean.c
+++ boolean.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, char*argv[]) {
-	char value1 = 0x60;
-	char value2 = 0x10;
+	char value1 = 0x80;
+	char value2 = 0x20;
 	
 	char result = value1 + value2;
 	
-	printf("[%x]value1: %d %x, \n", &value1, value1, value1);	
-	printf("[%x]value2: %d %x, \n", &value2, value2, value2);
-	printf("[%x]result: %d %x, \n", &result, result, result);
+	printf("[%x]value1: %d 0x%x\n", &value1, value1, value1);	
+	printf("[%x]value2: %d 0x%x\n", &value2, value2, value2);
+	printf("[%x]result: %d 0x%x\n", &result, result, result);
 	
 	return 0;
 }
