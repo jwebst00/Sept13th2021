15d11b0f12ea62587492222e37c5632a8bba5424
Jackson Webster <jwebst00@g.uafs.edu>
2021-09-13 15:52:08 +0000

    unsigned char

--- boolean.c
+++ boolean.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char*argv[]) {
-	char value1 = 0x80;
-	char value2 = 0x20;
+	unsigned char value1 = 0x80;
+	unsigned char value2 = 0x20;
 	
-	char result = value1 + value2;
+	unsigned char result = value1 + value2;
 	
 	printf("[%x]value1: %d 0x%x\n", &value1, value1, value1);	
 	printf("[%x]value2: %d 0x%x\n", &value2, value2, value2);
